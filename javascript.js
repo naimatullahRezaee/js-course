f004a2b338951c4fd8f59310c9a2235248937c49
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-07-03 14:36:19 +0200

    data type added

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+// we have two kinds of datatypes:
+// 1- primitive types:
+// string, number, boolean,undefined,null
+// 2 - Reference types:
+// object, array,function
+
+// Reference types:
+
+// Array :
+
+// let numbers = [1, 2, 3, 4, 5, "karim", true];
+// console.log(numbers);
+
+// Object :
+
+const person = {
+  name: "Ali",
+  age: 32,
+  gender: "Female",
+  address: {
+    country: "Afghanistan",
+    city: "Kabul",
+  },
+  hobbies: ["Learning", "Coding", "watching movies"],
+};
+
+person.email = "ali@gmail.com";
+console.log(person.address.city);
+console.log(person);
